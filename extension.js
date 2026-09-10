150a21ceef87c00800068bf3f02240ab1788a638
tiagocasalribeiro <79724578+tiagocasalribeiro@users.noreply.github.com>
2026-09-10 21:18:24 +0100

    fine tuning functionality, add vlan awareness

--- extension.js
+++ extension.js
@@ -3,7 +3,6 @@ import GLib from 'gi://GLib';
 import Clutter from 'gi://Clutter';
 import Soup from 'gi://Soup?version=3.0';
 import NM from 'gi://NM';
-import Gio from 'gi://Gio';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
@@ -12,8 +11,8 @@ export default class IpIndicatorExtension extends Extension {
         this._box = new St.BoxLayout({
             style_class: 'panel-button',
             y_align: Clutter.ActorAlign.CENTER,
-            reactive: true,
-            can_focus: true,
+            reactive: false,
+            can_focus: false,
             x_expand: false
         });
 
@@ -33,7 +32,7 @@ export default class IpIndicatorExtension extends Extension {
             text: "Local: ...", 
             y_align: Clutter.ActorAlign.CENTER,
             x_align: Clutter.ActorAlign.START,
-            style: 'min-width: 140px;'
+            style: 'min-width: 200px;'
         });
 
         this._box.add_child(this._publicIpLabel);
@@ -45,84 +44,203 @@ export default class IpIndicatorExtension extends Extension {
         this._lastPublicIp = '';
         this._lastLocalIp = '';
         this._pendingMessage = null;
+        
+        this._isVlanActive = false;
+        this._activeVlanId = null;
+        
+        this._isUpdating = false;
+        this._updateQueue = [];
+        this._refreshTimeoutId = null;
+        this._retryLocalId = null;
+        this._retryPublicId = null;
+        this._startupRetryIds = [];
+        this._deviceIp4Ids = new Map();
+        this._idleSourceId = null;
+        this._nmClient = null;
+        this._nmStateId = null;
+        this._nmActiveId = null;
+        this._timeoutId = null;
 
-        try {
-            this._nmClient = NM.Client.new(null);
-            this._nmStateId = this._nmClient.connect('notify::state', () => {
-                this._onNetworkStateChanged();
+        NM.Client.new_async(null, (client, result) => {
+            try {
+                this._nmClient = NM.Client.new_finish(result);
+                this._setupSignals();
+                this._scheduleRefresh();
+                this._startStartupPolling();
+            } catch (e) {
+                console.log(`[IP Indicator] NM Client async init failed: ${e}`);
+            }
+        });
+    }
+
+    _startStartupPolling() {
+        const delays = [0.5, 1, 2, 4, 8, 16];
+        delays.forEach(delay => {
+            const id = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, delay, () => {
+                this._scheduleRefresh();
+                return GLib.SOURCE_REMOVE;
             });
-        } catch (e) {
-            console.log(`[IP Indicator] Failed to initialize NM Client: ${e}`);
-        }
+            this._startupRetryIds.push(id);
+        });
+    }
 
-        this._updateLocalIp();
-        this._updatePublicIp();
-
-        this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 60, () => {
-            this._updateLocalIp();
-            this._updatePublicIp();
+    _setupSignals() {
+        if (!this._nmClient) return;
+        
+        this._nmStateId = this._nmClient.connect('notify::state', () => {
+            this._scheduleRefresh();
+        });
+        
+        this._nmActiveId = this._nmClient.connect('notify::active-connections', () => {
+            this._monitorDevicesIp4();
+            this._scheduleRefresh();
+        });
+        
+        this._monitorDevicesIp4();
+        
+        this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
+            this._scheduleRefresh();
             return GLib.SOURCE_CONTINUE;
         });
     }
 
+    _monitorDevicesIp4() {
+        if (!this._nmClient) return;
+        
+        for (const [device, signalId] of this._deviceIp4Ids) {
+            try { device.disconnect(signalId); } catch (e) {}
+        }
+        this._deviceIp4Ids.clear();
+        
+        const devices = this._nmClient.get_devices() || [];
+        for (const device of devices) {
+            const signalId = device.connect('notify::ip4-config', () => {
+                this._scheduleRefresh();
+            });
+            this._deviceIp4Ids.set(device, signalId);
+        }
+    }
+
     disable() {
-        // 1. Limpar temporizador
         if (this._timeoutId) {
             GLib.source_remove(this._timeoutId);
             this._timeoutId = null;
         }
         
-        // 2. Cancelar pedidos de rede pendentes
+        if (this._refreshTimeoutId) {
+            GLib.source_remove(this._refreshTimeoutId);
+            this._refreshTimeoutId = null;
+        }
+        
+        if (this._retryLocalId) {
+            GLib.source_remove(this._retryLocalId);
+            this._retryLocalId = null;
+        }
+        
+        if (this._retryPublicId) {
+            GLib.source_remove(this._retryPublicId);
+            this._retryPublicId = null;
+        }
+        
+        if (this._idleSourceId) {
+            GLib.source_remove(this._idleSourceId);
+            this._idleSourceId = null;
+        }
+        
+        for (const id of this._startupRetryIds) {
+            GLib.source_remove(id);
+        }
+        this._startupRetryIds = [];
+        
         this._cancelPendingRequest();
         
-        // 3. Desligar sinais do NetworkManager
+        for (const [device, signalId] of this._deviceIp4Ids) {
+            try { device.disconnect(signalId); } catch (e) {}
+        }
+        this._deviceIp4Ids.clear();
+        
         if (this._nmClient) {
-            if (this._nmStateId) {
-                this._nmClient.disconnect(this._nmStateId);
-            }
+            if (this._nmStateId) this._nmClient.disconnect(this._nmStateId);
+            if (this._nmActiveId) this._nmClient.disconnect(this._nmActiveId);
             this._nmClient = null;
         }
         
-        // 4. Destruir e libertar explicitamente todos os objetos criados (Corrige EGO-L-002 e EGO-L-005)
-        if (this._localIpLabel) {
-            this._localIpLabel.destroy();
-            this._localIpLabel = null;
-        }
-        if (this._separator) {
-            this._separator.destroy();
-            this._separator = null;
-        }
-        if (this._publicIpLabel) {
-            this._publicIpLabel.destroy();
-            this._publicIpLabel = null;
-        }
-        if (this._box) {
-            this._box.destroy();
-            this._box = null;
-        }
+        if (this._localIpLabel) { this._localIpLabel.destroy(); this._localIpLabel = null; }
+        if (this._separator) { this._separator.destroy(); this._separator = null; }
+        if (this._publicIpLabel) { this._publicIpLabel.destroy(); this._publicIpLabel = null; }
+        if (this._box) { this._box.destroy(); this._box = null; }
     }
 
     _cancelPendingRequest() {
         if (this._pendingMessage) {
-            try {
-                this._pendingMessage.cancel();
-            } catch (e) {
-                // Ignorar erros de cancelamento
-            }
+            try { this._pendingMessage.cancel(); } catch (e) {}
             this._pendingMessage = null;
         }
     }
 
-    _onNetworkStateChanged() {
-        const state = this._nmClient.get_state();
+    _scheduleRefresh() {
+        if (this._refreshTimeoutId) {
+            GLib.source_remove(this._refreshTimeoutId);
+        }
+        this._refreshTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, () => {
+            this._refreshTimeoutId = null;
+            this._executeRefresh();
+            return GLib.SOURCE_REMOVE;
+        });
+    }
+
+    _executeRefresh() {
+        if (this._isUpdating) {
+            this._updateQueue.push(() => this._executeRefresh());
+            return;
+        }
+
+        this._isUpdating = true;
+
+        try {
+            this._refreshAll();
+        } catch (e) {
+            console.log(`[IP Indicator] Error during refresh: ${e}`);
+        } finally {
+            this._isUpdating = false;
+            
+            if (this._updateQueue.length > 0) {
+                const nextUpdate = this._updateQueue.shift();
+                this._idleSourceId = GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+                    this._idleSourceId = null;
+                    nextUpdate();
+                    return GLib.SOURCE_REMOVE;
+                });
+            }
+        }
+    }
+
+    _refreshAll() {
+        const state = this._nmClient ? this._nmClient.get_state() : NM.State.UNKNOWN;
         
-        if (state === NM.State.CONNECTED_GLOBAL) {
-            this._updateLocalIp();
-            this._updatePublicIp();
-        } else {
+        if (state !== NM.State.CONNECTED_GLOBAL) {
             this._cancelPendingRequest();
+            this._cancelRetries();
+            this._isVlanActive = false;
+            this._activeVlanId = null;
             this._setLocalIpText('Local: 0.0.0.0');
             this._setPublicIpText('Public: 0.0.0.0');
+            return;
+        }
+
+        this._updateNetworkInfo();
+        this._updateLocalIp();
+        this._updatePublicIp();
+    }
+
+    _cancelRetries() {
+        if (this._retryLocalId) {
+            GLib.source_remove(this._retryLocalId);
+            this._retryLocalId = null;
+        }
+        if (this._retryPublicId) {
+            GLib.source_remove(this._retryPublicId);
+            this._retryPublicId = null;
         }
     }
 
@@ -140,39 +258,87 @@ export default class IpIndicatorExtension extends Extension {
         }
     }
 
-    _updateLocalIp() {
-        if (this._nmClient && this._nmClient.get_state() !== NM.State.CONNECTED_GLOBAL) {
-            this._setLocalIpText('Local: 0.0.0.0');
+    _getActiveLocalIp() {
+        if (!this._nmClient || this._nmClient.get_state() !== NM.State.CONNECTED_GLOBAL) {
+            return '0.0.0.0';
+        }
+
+        let targetAc = null;
+        const activeConnections = this._nmClient.get_active_connections() || [];
+
+        const vlanConns = activeConnections.filter(ac => 
+            ac && ac.connection && ac.connection.is_type(NM.SETTING_VLAN_SETTING_NAME)
+        );
+
+        if (vlanConns.length > 0) {
+            targetAc = vlanConns[0];
+        } else {
+            targetAc = this._nmClient.get_primary_connection();
+        }
+
+        if (!targetAc) return '0.0.0.0';
+
+        const devices = targetAc.get_devices() || [];
+        if (devices.length === 0) return '0.0.0.0';
+
+        const device = devices[0];
+        const ip4Config = device.get_ip4_config();
+
+        if (!ip4Config) return '0.0.0.0';
+
+        const addresses = ip4Config.get_addresses();
+        if (addresses && addresses.length > 0) {
+            return addresses[0].get_address();
+        }
+
+        return '0.0.0.0';
+    }
+
+    _updateNetworkInfo() {
+        if (!this._nmClient || this._nmClient.get_state() !== NM.State.CONNECTED_GLOBAL) {
+            this._isVlanActive = false;
+            this._activeVlanId = null;
             return;
         }
 
-        // Usar Gio.Subprocess assíncrono em vez de GLib.spawn_command_line_sync (Corrige EGO-X-002)
-        const proc = new Gio.Subprocess({
-            argv: ['hostname', '-I'],
-            flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
-        });
+        this._isVlanActive = false;
+        this._activeVlanId = null;
+
+        const activeConnections = this._nmClient.get_active_connections() || [];
         
-        try {
-            proc.init(null);
-            proc.communicate_utf8_async(null, null, (proc, res) => {
-                try {
-                    const [success, stdout, stderr] = proc.communicate_utf8_finish(res);
-                    if (success && stdout) {
-                        let ip = stdout.trim().split(' ')[0];
-                        if (!ip || ip === '') {
-                            ip = '0.0.0.0';
-                        }
-                        this._setLocalIpText(`Local: ${ip}`);
-                    } else {
-                        this._setLocalIpText('Local: 0.0.0.0');
-                    }
-                } catch (e) {
-                    this._setLocalIpText('Local: 0.0.0.0');
-                }
+        const vlanConns = activeConnections.filter(ac => 
+            ac && ac.connection && ac.connection.is_type(NM.SETTING_VLAN_SETTING_NAME)
+        );
+
+        if (vlanConns.length > 0) {
+            this._isVlanActive = true;
+            const vlanAc = vlanConns[0]; 
+            const vlanConn = vlanAc.get_connection();
+            const vlanSetting = vlanConn.get_setting_vlan();
+            
+            this._activeVlanId = vlanSetting ? vlanSetting.get_id() : null;
+        }
+    }
+
+    _updateLocalIp() {
+        const ip = this._getActiveLocalIp();
+        
+        let prefix = 'Local: ';
+        if (this._isVlanActive && this._activeVlanId !== null) {
+            prefix = `Local: [${this._activeVlanId}] `;
+        }
+        
+        this._setLocalIpText(`${prefix}${ip}`);
+        
+        if (ip === '0.0.0.0' && 
+            this._nmClient && 
+            this._nmClient.get_state() === NM.State.CONNECTED_GLOBAL &&
+            !this._retryLocalId) {
+            this._retryLocalId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
+                this._retryLocalId = null;
+                this._scheduleRefresh();
+                return GLib.SOURCE_REMOVE;
             });
-        } catch (e) {
-            console.log(`[IP Indicator] Failed to spawn hostname: ${e}`);
-            this._setLocalIpText('Local: 0.0.0.0');
         }
     }
 
@@ -183,9 +349,16 @@ export default class IpIndicatorExtension extends Extension {
         }
 
         this._cancelPendingRequest();
-        this._setPublicIpText('Public: ...');
+        
+        if (this._lastPublicIp === '' || 
+            this._lastPublicIp === 'Public: ...' || 
+            this._lastPublicIp === 'Public: 0.0.0.0') {
+            this._setPublicIpText('Public: ...');
+        }
 
         const session = new Soup.Session();
+        session.set_timeout(10);
+        
         this._pendingMessage = Soup.Message.new('GET', 'https://api.ipify.org?format=json');
         const currentMessage = this._pendingMessage;
         
@@ -197,11 +370,26 @@ export default class IpIndicatorExtension extends Extension {
 
             try {
                 const bytes = session.send_and_read_finish(result);
+                if (!bytes) throw new Error('No data received');
                 const text = new TextDecoder().decode(bytes.get_data());
                 const json = JSON.parse(text);
-                this._setPublicIpText(`Public: ${json.ip}`);
+                if (json && json.ip) {
+                    this._setPublicIpText(`Public: ${json.ip}`);
+                } else {
+                    throw new Error('Invalid response');
+                }
             } catch (e) {
-                this._setPublicIpText('Public: 0.0.0.0');
+                if (this._nmClient && 
+                    this._nmClient.get_state() === NM.State.CONNECTED_GLOBAL &&
+                    !this._retryPublicId) {
+                    this._retryPublicId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 2, () => {
+                        this._retryPublicId = null;
+                        this._updatePublicIp();
+                        return GLib.SOURCE_REMOVE;
+                    });
+                } else if (!this._retryPublicId) {
+                    this._setPublicIpText('Public: 0.0.0.0');
+                }
             } finally {
                 if (this._pendingMessage === currentMessage) {
                     this._pendingMessage = null;
